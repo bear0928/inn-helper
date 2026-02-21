979a34ecd2aca532527338a5b503b209318f80e8
Hsiung, Hsin-Ting <81227547+bear0928@users.noreply.github.com>
2026-02-21 06:31:50 +0000

    部署旅館客服系統v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 from deep_translator import GoogleTranslator
 
 # 網頁基礎設定
-st.set_page_config(page_title="喜園旅館客服系統", layout="wide")
+st.set_page_config(page_title="旅館客服系統", layout="wide")
 
 # --- 1. 資料處理函數 ---
 CSV_FILE = 'templates.csv'
